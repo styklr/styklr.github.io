0d4616847817674150b83897bd64394d835c2c7f
Ram <ram@spdeveloper.co.in>
2024-01-18 00:27:35 +0530

    Updated contact info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2236,6 +2236,7 @@ xmlns="http://www.w3.org/2000/svg" fill="none" stroke-width="1.5" stroke="curren
             KARNATAKA, 560035<br/>
             MOBILE: +91 9739280208<br/>
             EMAIL: ram@styklr.com
+            https://www.styklr.com
                     </p>
                     <p className="text-base text-body-color">
                     29-6-1, First Floor,
